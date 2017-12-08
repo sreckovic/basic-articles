1cc03583b7afa8adf2054fa4333d9076457682be
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-12-08 17:39:21 +0100

    Single Post
    Single Post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -35,7 +35,7 @@ class Blog extends Component {
         {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Route path="/posts/:id" exact component={FullPost} />
       </div>
     );
   }
--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,13 +8,15 @@ class FullPost extends Component {
     loadedPost: null
   };
 
-  componentDidUpdate() {
-    if (this.props.id) {
+  componentDidMount() {
+    console.log(this.props);
+    if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
-        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+        (this.state.loadedPost &&
+          this.state.loadedPost.id !== this.props.match.params.id)
       ) {
-        axios.get("/posts/" + this.props.id).then(response => {
+        axios.get("/posts/" + this.props.match.params.id).then(response => {
           const post = response.data;
           this.setState({ loadedPost: post });
           //console.log(response);
@@ -24,7 +26,7 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete("/posts/" + this.props.id).then(response => {
+    axios.delete("/posts/" + this.props.match.params.id).then(response => {
       console.log(response);
     });
   };
@@ -32,7 +34,7 @@ class FullPost extends Component {
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
 
-    if (this.props.id) {
+    if (this.props.match.params.id) {
       let post = <p style={{ textAlign: "center" }}>Loading...</p>;
     }
 
