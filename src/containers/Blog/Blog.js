bbebcbe16a09c31e5002a6f9dec52ad3ab81880a
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-12-11 16:57:18 +0100

    asyncComponent
    asyncComponent

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -50,6 +50,7 @@ class Blog extends Component {
             <Route path="/new-post" component={NewPost} />
           ) : null}
           <Route path="/posts" component={Posts} />
+          <Route render={() => <h1>Not found</h1>} />
           {/*<Redirect from="/" to="/posts" />*/}
           {/*<Route path="/" component={Posts} />*/}
         </Switch>
